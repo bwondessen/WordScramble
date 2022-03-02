2309a9b66060687dc0bc70faf4da0f1b785a8b1e
Bruke Wondessen <bwondessen23@gmail.com>
2022-03-02 16:36:26 -0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Bruke on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
